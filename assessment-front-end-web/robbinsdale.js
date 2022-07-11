0e10d2b16f59770a5a9cf74abde26f16cef34cd8
Alegra Leeson <103084730+Alegraleeson@users.noreply.github.com>
2022-07-11 09:15:00 -0500

    Add files via upload

--- assessment-front-end-web/robbinsdale.js
+++ assessment-front-end-web/robbinsdale.js
@@ -2,11 +2,12 @@ console.log('connection successful');
 
 let pickBtn = document.querySelector('.picker');
 let restaurants = document.querySelectorAll('.res');
+let resPick = ['Broadway Pizza', `Marna's Eatery`, `Athen's Cafe`, 'Birdhouse Eat & Drink', `Mama Lupita's Cafe and Grill`]
 
 console.log(restaurants);
 
 function pickRestaurant(event){
-    alert(`Try ` +  restaurants[Math.floor(Math.random() * restaurants.length)]);
+    alert(`Try ` +  resPick[Math.floor(Math.random() * resPick.length)]);
 }
 
 // for(i=0;i<restaurants.length;i++){
