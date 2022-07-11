9b0b3023587688c43cb143d02488b0e5941ffa53
Alegra Leeson <103084730+Alegraleeson@users.noreply.github.com>
2022-07-11 00:24:28 -0500

    Add files via upload

--- assessment-front-end-web/robbinsdale.js
+++ assessment-front-end-web/robbinsdale.js
@@ -6,7 +6,7 @@ let restaurants = document.querySelectorAll('.res');
 console.log(restaurants);
 
 function pickRestaurant(event){
-    alert(restaurants[Math.floor(Math.random() * restaurants.length)]);
+    alert(`Try ` +  restaurants[Math.floor(Math.random() * restaurants.length)]);
 }
 
 // for(i=0;i<restaurants.length;i++){
